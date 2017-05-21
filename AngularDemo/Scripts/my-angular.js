5154b61e8457d715c1c5bd38b865533bf3fb44dd
Besim Alataş <alatas.besim@gmail.com>
2017-05-22 00:29:38 +0300

    proje is created and Begining of the master page

---
+++ AngularDemo/Scripts/my-angular.js
@@ -0,0 +1,18 @@
+﻿
+var application =angular.module("myApp", ["ngRoute"]);
+
+application.config(function ($routeProvider,$locationProvider) {
+
+    $routeProvider
+        .when('/index1',
+    {
+        templateUrl: "/Templates/Indexsad1.html"
+    });
+
+    $locationProvider.html5Mode(false).hashPrefix("!");
+});
+
+application.controller("myController",
+    function($scope) {
+       
+    });
